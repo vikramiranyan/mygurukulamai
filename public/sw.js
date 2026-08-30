ad04775d3ca90af55e822b8edf2bc286abd122f6
Vikram Iranyan <VIKRAM.IRANYAN@GMAIL.COM>
2026-08-30 10:08:37 +0530

    Refresh PWA cache for refined app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='gurukulam-ai-v3';
+const CACHE='gurukulam-ai-v4';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
