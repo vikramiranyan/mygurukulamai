6e3d5b7e5e46dbeaef1f9d1d722441f809b2762d
Vikram Iranyan <VIKRAM.IRANYAN@GMAIL.COM>
2026-08-31 07:03:28 +0530

    Test flat PDF timetable extraction

--- src/timetable/parser.test.ts
+++ src/timetable/parser.test.ts
@@ -30,4 +30,24 @@ describe('timetable parser', () => {
     expect(periods).toContainEqual(expect.objectContaining({ day: 'Tuesday', start: '10:10', end: '10:45', subject: 'Skating' }));
     expect(periods).toContainEqual(expect.objectContaining({ day: 'Tuesday', start: '10:45', end: '11:05', subject: 'Lunch', type: 'break' }));
   });
+
+  it('parses a PDF extractor that flattens the whole timetable onto one line', () => {
+    const text = [
+      'IA1 8:00-8:15 am 8:15-8:25 am 8:25-9:00 am 9:00-9:35 am 9:35-10:10 am 10:10-10:45 am 10:45-11:05 11:05-11:45 am 11:45-12:25 pm 12:25-1:05 pm 1:05-1:45 pm DAYS MS SIMRAN MS SIMRAN I II III IV V VI VII DEAR/D L/ DISPERSAL',
+      'MON Assembly Time Fruit Break ENGLISH (Course Book) COMPUTER HINDI (Notebook) TK LUNCH DRAWING EVS (Notebook) MATHS (Course Book) DEAR',
+      'TUES Assembly Time Fruit Break ENGLISH (Notebook) COMPUTER HINDI (Course Book) SKATING LUNCH EVS (Course Book) EVS MATHS (Notebook) DEAR',
+      'WED Assembly Time Fruit Break ENGLISH (Course Book) ROBOTICS HINDI (Notebook) CACA LUNCH MATHS (Course Book) EVS (Notebook) MATHS DEAR',
+      'THURS Assembly Time Fruit Break ENGLISH (Notebook) LANGUAGE LAB HINDI (Course Book) P.E. LUNCH DANCE EVS (Course Book) MATHS (Notebook) DEAR',
+      'FRI Assembly Time Fruit Break ENGLISH (Course Book) YOGA HINDI (Notebook) GK LUNCH ENGLISH (Course Book) EVS (Notebook) MATHS (Course Book) DEAR',
+      'SAT Assembly Time Fruit Break ENGLISH (Workbook) DRAWING HINDI (Workbook) HINDI (Workbook) LUNCH MUSIC EVS (Workbook) MATHS (Wise time) DEAR',
+    ].join(' ');
+    const periods = parseTimetableText(text);
+    const subjects = extractSubjects(periods).sort();
+    expect(periods).toHaveLength(66);
+    expect(subjects).toEqual(['CACA', 'Computer', 'DEAR', 'Dance', 'Drawing', 'EVS', 'English', 'GK', 'Hindi', 'Maths', 'Music', 'P.E.', 'Robotics', 'Skating', 'TK', 'Yoga']);
+    expect(subjects).not.toContain('Assembly Time');
+    expect(subjects).not.toContain('Fruit Break');
+    expect(subjects).not.toContain('Lunch');
+    expect(periods).toContainEqual(expect.objectContaining({ day: 'Thursday', start: '09:00', end: '09:35', subject: 'Language Lab' }));
+  });
 });
